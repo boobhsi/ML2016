0b6f621b5e34d39cd899303f6796876bb89c88c4
Li-Ming-Hsi <b02901065@ntu.edu.tw>
2016-10-14 19:10:59 +0800

    commit hw1

--- hw1/linear_regression.sh
+++ hw1/linear_regression.sh
@@ -1,2 +1,2 @@
-python src/main.py data/train.csv 300000 data/test_X.csv ./kaggle_best_result.csv 1 2.25e-10 0 0
+python src/main.py data/train.csv 300000 data/test_X.csv ./linear_regression.csv 1 2.25e-10 0 0
 
