6da5ca93c6cbb063c0f06d38332363161614e1a3
Li-Ming-Hsi <b02901065@ntu.edu.tw>
2016-10-28 20:31:19 +0800

    hw2 submission

--- hw0/q2.py
+++
@@ -1,5 +0,0 @@
-from PIL import Image
-import sys
-
-im = Image.open(sys.argv[1])
-im.rotate(180).save("ans2.png")
--- hw1/linear_regression.sh
+++
@@ -1,2 +0,0 @@
-python src/main.py data/train.csv 300000 data/test_X.csv ./linear_regression.csv 1 2.25e-10 0 0
-
