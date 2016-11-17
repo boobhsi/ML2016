63a9f497b2828c9df4736242c4ab8450e918302d
Li-Ming-Hsi <b02901065@ntu.edu.tw>
2016-11-17 11:59:21 +0800

    005

--- hw3/src/main.py
+++ hw3/src/main.py
@@ -29,9 +29,10 @@ aul = None
 
 al = pickle.load(open("data/all_label.p", "rb"))
 #if int(argv[2])!=0: testing_data = pickle.load(open("data/test.p", "rb"))
-if int(argv[2])!=0: aul = pickle.load(open("data/all_unlabel.p", "rb"))
+if int(argv[2])!=0: auld = pickle.load(open("data/all_unlabel.p", "rb"))
 class_count = len(al)
 count_per_class = len(al[0])
+count_for_ul = len(auld)
 
 #print testing_data
 
@@ -59,7 +60,7 @@ for i in range(class_count):
         vali_ans = np.concatenate((vali_ans, temp_ans_for_vali), axis=0)
 
 if int(argv[2])!=0:
-    aul = np.array(aul).reshape(45000, 3, 32, 32)
+    aul = np.array(auld).reshape(count_for_ul,  3, 32, 32)
 
 training_data = np.array(training_data)
 vali_data = np.array(vali_data)
@@ -82,15 +83,15 @@ model.add(Activation("relu"))
 model.add(MaxPooling2D((2,2)))
 #model.add(Dropout(0.25))
 model.add(Flatten())
-model.add(Dense(output_dim=60))
+model.add(Dense(output_dim=100))
 model.add(Activation("sigmoid"))
 model.add(Dropout(0.5))
+#model.add(Dense(output_dim=100))
+#model.add(Activation("relu"))
+#model.add(Dropout(0.5))
 #model.add(Dense(output_dim=60))
 #model.add(Activation("relu"))
 #model.add(Dropout(0.5))
-#model.add(Dense(output_dim=200))
-#model.add(Activation("relu"))
-#model.add(Dropout(0.3))
 #model.add(Dense(output_dim=200))
 #model.add(Activation("relu"))
 #model.add(Dropout(0.3))
@@ -111,7 +112,7 @@ model.add(Dense(output_dim=100))
 model.add(Activation("relu"))
 """
 
-ao = Adam(lr=0.00005)
+ao = Adam(lr=0.0001)
 rp = RMSprop(lr=0.0005)
 model.add(Dense(output_dim=10))
 model.add(Activation("softmax"))
@@ -125,27 +126,29 @@ val_acc_max = -1000.
 val_loss_min = 1000.
 self_counter = 1
 switch = 1
+control = int(argv[3])
 
 def fit_model(tx, ty):
-    max_time = 10000
+    max_time = 1000
     global cons_non_decay
     global val_acc_max
     global val_loss_min
     global self_counter
     global switch
+    global control
     while(max_time > 0):
         print "{0}th epoch, continuous overfitting: {1}".format(self_counter, cons_non_decay)
-        print "current best val_acc = {0}".format(val_acc_max)
+        print "current best val_loss_min = {0}".format(val_loss_min)
         #hist = model.fit_generator(datagen.flow(tx, ty, batch_size=int(argv[1])*3), nb_epoch=1, validation_data=(vali_data, vali_ans), samples_per_epoch=training_data.shape[0]*3)
-        hist = model.fit(tx, ty, batch_size=int(argv[1])*switch, nb_epoch=1, validation_data=(vali_data, vali_ans))
+        hist = model.fit(tx, ty, batch_size=int(argv[1]) * switch, nb_epoch=1, validation_data=(vali_data, vali_ans))
         print hist.history
         self_counter += 1
-        if hist.history["val_acc"][0] > val_acc_max:
+        if hist.history["val_loss"][0] < val_loss_min - 0.005:
             model.save(argv[4])
             cons_non_decay = 0
-            val_acc_max = hist.history["val_acc"][0]
+            val_loss_min = hist.history["val_loss"][0]
         else: cons_non_decay += 1
-        if cons_non_decay == int(argv[3]) and int(argv[3]) != -1: break
+        if cons_non_decay == control and control != -1: break
         max_time -= 1
         if max_time == 0: break
 
@@ -154,9 +157,9 @@ if int(argv[2])==0:
 
 elif int(argv[2])==1:
     fit_model(training_data, training_ans)
-    switch *= 20
+    switch *= 10
     while(1):
-        judge = val_acc_max
+        judge = val_loss_min
         cons_non_decay = 0
         del model
         model = load_model(argv[4])
@@ -164,37 +167,34 @@ elif int(argv[2])==1:
         new_ans = model.predict(aul)
         new_class = model.predict_classes(aul)
         new_l_counter = 0
-        new_tx = None
         new_ty = []
-
+        new_tx = []
         for i in range(aul.shape[0]):
-            ul_tobe_l = np.expand_dims(aul[i], axis=0)
-            if random.random() < new_ans[i][new_class[i]]:
-                temp = [0] * class_count
-                temp[new_class[i]] = 1
-                temp = np.expand_dims(np.array(temp), axis=0)
-                if not isinstance(new_ty, np.ndarray):
-                    new_tx = ul_tobe_l
-                    new_ty = temp
-                else:
-                    #print ul_tobe_l.shape
-                    #print temp.shape
-                    new_tx = np.concatenate((new_tx, ul_tobe_l), axis=0)
-                    new_ty = np.concatenate((new_ty, temp), axis=0)
+            if random.random() < new_ans[i][new_class[i]] and new_ans[i][new_class[i]] > 0.5:
+                temp_l = [0] * 10
+                temp_l[new_class[i]] = 1
+                new_ty = new_ty + [temp_l]
+                new_tx = new_tx + [auld[i]]
                 new_l_counter += 1
-            print i
-            temp = [0] * class_count
-            temp[new_class[i]] = 1
-            new_ty = new_ty + [temp]
-
-        #print "There are {0} new data".format(new_l_counter)
+            #else:
+            #    aul[i] = aul[i] * 0
+            #temp_l[i][new_class[i]] = 1
+        print "\nThere are {0} new data".format(new_l_counter)
+        new_tx = np.array(new_tx)
         new_ty = np.array(new_ty)
+        new_tx = new_tx.reshape(new_tx.shape[0], 3, 32, 32)
+        #print new_tx.shape
         #print new_ty.shape
         new_tx = np.concatenate((training_data, new_tx), axis=0)
         new_ty = np.concatenate((training_ans, new_ty), axis=0)
-
+        ao = Adam(lr=0.0005)
+        model.compile(loss="categorical_crossentropy",
+              optimizer=ao,
+              metrics=["accuracy"])
+        val_loss_min = 1000.
         fit_model(new_tx, new_ty)
-        if val_acc_max <= judge: break
+        model.save(argv[4])
+
 del model
 model = load_model(argv[4])
 td = pickle.load(open("data/test.p", "rb"))
